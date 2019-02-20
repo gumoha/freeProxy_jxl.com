f4b6edeaeaff455d563bac0341ab65f034026191
gumo <gumoha@outlook.com>
2019-02-20 18:13:58 +0800

    http://ip.jiangxianli.com/

--- freeip/freeip/pipelines.py
+++ freeip/freeip/pipelines.py
@@ -11,30 +11,16 @@ from scrapy.exceptions import DropItem
 class FreeipPipeline(object):
     def open_spider(self,spider):
         filen = "F:\Scrapy\IP_jiangxianli\-FreeIP-.json"
-        self.file = codecs.open(filen,'a')
+        self.file = codecs.open(filen,'w')
 
     def close_spider(self,spider):
         self.file.close()
 
     def process_item(self, item, spider):
-        print('验证搜集到的IP……')
-        ip = '{0}://{1}:{2}'.format(item['protocol'],item['ip'],item['port'])
-        if self.ipCheck(ip) is True:
-            print('此ip：%s 验证可用，保存！' % ip)
-            line = json.dumps(dict(ip), ensure_ascii=False) + '\n'
-            self.file.write(line)
-        else:
-            print('此ip：%s 验证不可用，丢弃！' % ip)
-            raise DropItem(item)
+        #ip = '{0}://{1}:{2}'.format(item['protocol'],item['ip'],item['port'])
+        ip = {'{0}'.format(item['protocol']): '{0}:{1}'.format(item['ip'], item['port'])}
+
+        line = json.dumps(dict(ip), ensure_ascii=False) + '\n'
+        self.file.write(line)
 
         return item
-
-    def ipCheck(self,ip):
-        checkurl = 'https://cd.lianjia.com/ershoufang/'
-
-        try:
-            req = requests.get(checkurl,proxies = ip,timeout=3)
-            if req.status_code == 200:
-                return True
-        except:
-            return False
